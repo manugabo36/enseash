43865fb1917b9b1e6c68b33a1cde517a876cec71
Manuel <manuel.gabon01@gmail.com>
2023-11-27 09:57:41 +0100

    question3

--- Question3.c
+++ Question3.c
@@ -13,9 +13,13 @@ int main() {
     char* input;
 
     while(1){
+        //write a different message depending on the input message
         scanf("%s",input);
-        if(strcmp(input,"fortune")==0){     //write a different message depending on the input message
+        if(strcmp(input,"fortune")==0){
             write(1,"Today is what happened to yesterday\n",strlen("Today is what happened to yesterday\n"));
+        }
+        else if (strcmp(input,"exit")==0) { //quit the shell if the command is exit
+            exit(0);
         }
         else{write(1 ,"Try again \n", strlen("Try again \n"));
         }
